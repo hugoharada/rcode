b15c318af91bf99884cdcf2880f27b55b549d4eb
Hugo Harada <hugo.harada@gmail.com>
2018-07-12 00:22:44 -0300

    simulacao de distribuicao amostral

---
+++ code/sim/0001-DistribuicoesAmostrais/DistribuicaoAmostral.r
@@ -0,0 +1,28 @@
+
+# veja magalhães p. 237 para referências.
+
+n1<-100
+for(n in c(10,20,30,50,100,500,1000,5000)){
+  med <- c();
+  dp_a <- c();
+  dp_p <- c();
+  i <- 1
+  while(i<n1) {
+    amostra <- rnorm(n,5,10)
+    med <- c(med,mean(amostra)) #coleção de médias
+    i <- i + 1
+  }
+  print("===================="); 
+  print(sprintf("n= %d", n ))
+  print(sprintf("media de X_bar= %f",mean(med))); #média das médias
+  print(sprintf("desvio padrão de X_bar= %f",sd(med))); #desvio padrão da coleção de médias
+  print(sprintf("desvio padrão de população = %f",sd(med)*sqrt(n))); #desvio padrão da população que originou as médias.
+}
+
+
+
+
+
+
+
+
