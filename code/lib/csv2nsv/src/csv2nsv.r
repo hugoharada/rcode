4bec47a0f236c2cd19a6e172592f7964473a758d
Hugo Harada <hugo.harada@gmail.com>
2018-07-12 00:15:38 -0300

    Initial commit

---
+++ code/lib/csv2nsv/src/csv2nsv.r
@@ -0,0 +1,17 @@
+
+##########################################################################################
+#	Author: Hugo Kenji Pereira Harada
+#
+#	Name: csv2nsa.r
+#
+#	Purpose: Comma separated valeus to not separated values
+#          Ex: A,B,C,D,E --> ABCDE
+#
+#	Use Example
+# 
+#	Commit History: 
+#		$Header$: 
+#
+##########################################################################################
+
+
---
+++ code/lib/nsv2csv/src/nsv2csv.r
@@ -0,0 +1,19 @@
+
+##########################################################################################
+#	Author: Hugo Kenji Pereira Harada
+#
+#	Name: nsa2csa.r
+#
+#	Purpose: not separated values to conma separated valeus to 
+#          Ex: ABCDE --> A,B,C,D,E
+#
+#	Use Example:
+# 
+#	Commit History: 
+#		$Header$: 
+#
+##########################################################################################
+
+y <-"ABCDE"
+
+y<-strsplit(y,"")
