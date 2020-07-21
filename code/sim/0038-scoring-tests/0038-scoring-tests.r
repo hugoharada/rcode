304b54578229ea060490ed090528872dc1460d07
Hugo Harada <hugo.harada@gmail.com>
2020-07-21 15:56:35 -0300

    added study on pbis calculation. Note difference on how CTT and PerFit and ltm calculate pbis

--- code/sim/0038-scoring-tests/0038-scoring-tests.r
+++ code/sim/0038-scoring-tests/0038-scoring-tests.r
@@ -24,16 +24,22 @@ scoring<-function(data_matrix, key_vector ){
 }
 
 calc_stats <-function(scored_data){
-  scores <- rowSums(scored_data)
+  score <- rowSums(scored_data)
   return(list(
-    scores = scores,
+    scores = score,
     nPerson = nrow(scored_data),
     nItem = ncol(scored_data),
-    scaleMean = mean(scores),
-    scaleSD = sd(scores),
+    scaleMean = mean(score),
+    scaleSD = sd(score),
     itemMean = colSums(scored_data)/nrow(scored_data)
   ))
 }
+
+pbis_calc <-function(scored_data_matrix,score_vector){
+  tmp <- cbind(scored_data_matrix,score_vector)
+  
+}
+
 
 create_ctt_score_struct <- function(data_matrix, key_vector){
   scored_data = scoring(data_matrix, key_vector)
@@ -54,12 +60,6 @@ create_ctt_score_struct <- function(data_matrix, key_vector){
   )
 }
 
-list(
-  a = 1,
-  b = list (c =3,d =6)
-)
-
-
 scoring_vector(key_vector=key, answer_vector=answer[3,])
 
 t(apply(X=answer, MARGIN = 1,FUN=scoring_vector, key_vector = key))
@@ -68,11 +68,43 @@ scored<-scoring(data_matrix = answer, key_vector = key)
 scores <- rowSums(scored)
 calc_stats(scored)
 
-lc_esp_em3_raw_scores$reliability$nPerson #person number
-lc_esp_em3_raw_scores$reliability$nItem #item
-lc_esp_em3_raw_scores$reliability$scaleMean #item
-lc_esp_em3_raw_scores$reliability$scaleSD #item
-lc_esp_em3_raw_scores$reliability$pBis #item
-lc_esp_em3_raw_scores$reliability$itemMean #item
+n1 <- sum(scored[,3]==1)
+n0 <- sum(scored[,3]==0)
+m1 <- mean(scores[scored[,3]==1])
+m0 <- mean(scores[scored[,3]==0])
+m <- mean(scores)
+n <- n1+n0
+sd_bias <- sd(scores)*sqrt((n-1)/n) # using biased sd
+sd_unbias <- sd(scores)
+(m1-m)/sd_bias*sqrt(n1/n0)       #point biserial - using biased sd
+(m1-m0)/sd_bias*sqrt(n1*n0/n^2) #point biserial  - using biased sd
+(m1-m0)/sd_unbias*sqrt(n1*n0/n/(n-1)) #point biserial - using unbiased sd
+
+(m1-m)/sd*sqrt(n1/n/dnorm(n1/n)) #biserial
 
 
+n1 <- sum(scored[,3]==1)
+n0 <- sum(scored[,3]==0)
+scores_tmp <- scores-scored[,3]
+
+m1 <- mean(scores_tmp[scored[,3]==1])
+m0 <- mean(scores_tmp[scored[,3]==0])
+m <- mean(scores_tmp)
+n <- n1+n0
+sd_bias <- sd(scores_tmp)*sqrt((n-1)/n) # using biased sd
+sd_unbias <- sd(scores_tmp)
+(m1-m)/sd_bias*sqrt(n1/n0)       #point biserial - using biased sd
+(m1-m0)/sd_bias*sqrt(n1*n0/n^2) #point biserial  - using biased sd
+(m1-m0)/sd_unbias*sqrt(n1*n0/n/(n-1)) #point biserial - using unbiased sd
+
+(m1-m)/sd*sqrt(n1/n/dnorm(n1/n)) #biserial
+
+
+
+
+if(!require(PerFit)) install.packages("PerFit"); library(PerFit)
+PerFit::r.pbis(t(scored))
+if(!require(CTT)) install.packages("CTT"); library(CTT)
+
+x<-CTT::score(items=answer,key=key,rel=TRUE,output.scored=TRUE)
+str(x)
