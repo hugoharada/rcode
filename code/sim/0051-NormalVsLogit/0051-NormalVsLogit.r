78f8539fbc894d5552b33417799862946a59a51e
Hugo Harada <hugo.harada@gmail.com>
2020-08-05 17:57:42 -0300

    initial commit

---
+++ code/sim/0051-NormalVsLogit/0051-NormalVsLogit.r
@@ -0,0 +1,46 @@
+library(lavaan)
+library(ggplot2)
+
+data(HolzingerSwineford1939)
+
+## fit model
+HS.model <- ' visual  =~ x1 + x2 + x3
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 '
+
+fit <- cfa(HS.model, data = HolzingerSwineford1939)
+head(lavPredict(fit))
+head(lavPredict(fit, type = "ov"))
+
+N<-1000
+xnorm <- rnorm(N,mean = 1, sd=1)
+pxnorm <-pnorm(xnorm,mean = 1, sd=1)
+pxlogis <- plogis(xnorm,location = 1, scale=1)
+
+data <- data.frame(rbind(
+  cbind(xnorm,pxnorm,rep("normal",N)),
+  cbind(xnorm,pxlogis,rep("logit",N))
+))
+colnames(data)<-c("x","p","dist")
+head(data)
+str(data)
+data$x <- as.numeric(as.character(data$x))
+data$p <- as.numeric(as.character(data$p))
+ggplot(data, aes(x=x,y=p,colours=dist))+geom_line()+geom_vline(xintercept =  1)
+
+xnorm <- rnorm(N,mean = 1, sd=1)
+pxnorm <-pnorm(xnorm,mean = 1, sd=1.702)
+pxlogis <- plogis(xnorm,location = 1, scale=1)
+
+data <- data.frame(rbind(
+  cbind(xnorm,pxnorm,rep("normal",N)),
+  cbind(xnorm,pxlogis,rep("logit",N))
+))
+colnames(data)<-c("x","p","dist")
+head(data)
+str(data)
+data$x <- as.numeric(as.character(data$x))
+data$p <- as.numeric(as.character(data$p))
+ggplot(data, aes(x=x,y=p,colours=dist))+geom_line()
+
+
